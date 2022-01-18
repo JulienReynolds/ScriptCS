3247b70879a0314c72486f18039615f5ecd6b57a
JulienReynolds <97959789+JulienReynolds@users.noreply.github.com>
2022-01-18 18:24:48 +0100

    Add files via upload

---
+++ ia.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ia : MonoBehaviour {
+    Transform playerTransform;
+    private float NextCheck;
+    UnityEngine.AI.NavMeshAgent myNavMesh;
+    public float CheckRate = 0.01f;
+    
+
+
+    void Start () {
+        if(GameObject.FindGameObjectWithTag("Player"))
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        myNavMesh= gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+    void Update () {
+       if (Time.time > NextCheck)
+       {
+      NextCheck = Time.time + CheckRate;
+       }
+FollowPlayer();
+
+    }
+    void FollowPlayer () {
+        {
+        myNavMesh.transform.LookAt(playerTransform);
+        myNavMesh.destination = playerTransform.position;
+    }
+}
+}
