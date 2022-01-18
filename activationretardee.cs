5b7d4737339c2f7780287d85755010d372df973e
JulienReynolds <97959789+JulienReynolds@users.noreply.github.com>
2022-01-18 18:22:18 +0100

    Add files via upload

---
+++ activationretardee.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+using MFPS.PlayerController;
+using UnityEngine.Serialization;
+public class activationretardee : MonoBehaviour{
+
+public GameObject go;
+
+void Start (){
+go.SetActive( false );
+StartCoroutine(ExampleCoroutine());
+ExampleCoroutine();
+}
+
+IEnumerator ExampleCoroutine(){
+        
+        yield return new WaitForSeconds(2);
+go.SetActive( true );
+
+ }
+}
