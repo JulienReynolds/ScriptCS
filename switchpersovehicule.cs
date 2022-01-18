f047d01fc8aad55f5cf8475987fa702e0509fa10
JulienReynolds <97959789+JulienReynolds@users.noreply.github.com>
2022-01-18 18:13:40 +0100

    Add files via upload

---
+++ switchpersovehicule.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class switchpersovehicule : MonoBehaviour {
+
+	public GameObject boat;
+	public GameObject boatCamera;
+	public GameObject player;
+	public GameObject playerStartPos;
+
+	void Start () {
+	
+	}
+	
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.E)){
+		{
+			boat.GetComponent<Rigidbody>().isKinematic = false;
+			boat.GetComponent<boat>().enabled = true;
+			boatCamera.SetActive(true);
+
+			player.SetActive(false);
+		}
+
+		if(Input.GetKey(KeyCode.F)){
+		{
+			boat.GetComponent<Rigidbody>().isKinematic = true;
+			boat.GetComponent<boat>().enabled = false;
+			boatCamera.SetActive(false);
+
+			player.SetActive(true);
+			player.transform.position = playerStartPos.transform.position;
+		}
+	}
+}
